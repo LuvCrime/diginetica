88f2d74c1d80666823ffcf8439066f270035548d
Ostrovskaya Alexandra <59867572+LuvCrime@users.noreply.github.com>
2021-02-12 22:59:51 +0300

    add js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let categoriesData = {
     name: "Название категории",
     quantity: "134 товара",
-    img: `<svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-    </svg>`,
   };
 
   let categoriesSidebarData = {
@@ -13,143 +10,302 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   let productData = [
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Рюкзак",
       price: 1000,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Рюкзак",
       price: 2000,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Рюкзак",
       price: 3000,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Чехол",
       price: 100,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Чехол",
       price: 150,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Чехол",
       price: 500,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Тумбочка",
       price: 3000,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Тумбочка",
       price: 3800,
     },
     {
-      img: `<svg width="260" height="260" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="60" height="60" rx="3" fill="#C4C4C4"/>
-      </svg>`,
       name: "Тумбочка",
       price: 4500,
     },
+    {
+      name: "Часы",
+      price: 10000,
+    },
+    {
+      name: "Часы",
+      price: 11000,
+    },
+    {
+      name: "Часы",
+      price: 12000,
+    },
+    {
+      name: "Тумбочка",
+      price: 3000,
+    },
+    {
+      name: "Тумбочка",
+      price: 3800,
+    },
+    {
+      name: "Тумбочка",
+      price: 4500,
+    },
+    {
+      name: "Часы",
+      price: 10000,
+    },
+    {
+      name: "Часы",
+      price: 11000,
+    },
+    {
+      name: "Часы",
+      price: 12000,
+    },
   ];
+
+  let currentArray = productData;
+  let currentSort = sortDefault;
+  let currentPage = 1;
+  let currentPageSize;
+  let priceFrom;
+  let priceTo;
+  let discount = 0.9;
 
   document.querySelector(".focus-default").focus();
 
-  let templateCategory = document.querySelector(".category-item");
+  renderSlider();
 
-  let templateCategoriesSidebar = document.querySelector(
-    ".all-categories-sidebar"
-  );
+  clearCheckbox();
 
-  let clearCheckboxButton = document.querySelector(".clear");
+  initPageSize();
 
-  let templateProductCard = document.querySelector(".product-card");
+  renderCategories();
 
-  for (let i = 0; i < 5; i++) {
-    let clone = document.importNode(templateCategory.content, true);
-    Object.keys(categoriesData).forEach((key) => {
-      if (key === "img") {
-        clone.querySelector(".item-" + key).innerHTML = categoriesData[key];
-      } else {
-        clone.querySelector(".item-" + key).textContent = categoriesData[key];
+  renderSidebar();
+
+  initSearch();
+
+  initPagination();
+
+  initPriceRange();
+
+  initSort();
+
+  renderProductsPage(1);
+
+  function initSort() {
+    let sortButtons = document.querySelector(".catalog-sorter");
+
+    sortButtons.addEventListener("click", function (e) {
+      e.preventDefault();
+      currentSort = e.target.dataset.sort;
+      switch (e.target.dataset.sort) {
+        case "cheap":
+          currentSort = sortAscending;
+          break;
+        case "expensive":
+          currentSort = sortDescending;
+          break;
+        case "default":
+        default:
+          currentSort = sortDefault;
+          break;
+      }
+      renderProducts();
+    });
+  }
+
+  function sortAscending(a, b) {
+    return a.price - b.price;
+  }
+
+  function sortDescending(a, b) {
+    return b.price - a.price;
+  }
+
+  function sortDefault(a, b) {
+    return 0;
+  }
+
+  function renderSlider() {
+    let slider = document.getElementById("slider");
+
+    noUiSlider.cssClasses.target += " range-slider";
+
+    noUiSlider.create(slider, {
+      start: [20, 80],
+      connect: true,
+      range: {
+        min: 0,
+        max: 100,
+      },
+    });
+  }
+
+  function initPageSize() {
+    let select = document.querySelector(".select-val");
+    currentPageSize = select.value;
+    select.addEventListener("change", function (e) {
+      currentPageSize = select.value;
+      renderProducts();
+    });
+  }
+
+  function clearCheckbox() {
+    let clearCheckboxButton = document.querySelector(".clear");
+
+    clearCheckboxButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      let checkboxProducts = document.querySelectorAll(".brand-item");
+      for (let i = 0; i < checkboxProducts.length; i++) {
+        checkboxProducts[i].checked = false;
       }
     });
-    let productCategories = document.querySelector(".product-categories");
-    productCategories.appendChild(clone);
   }
 
-  for (let i = 0; i < 8; i++) {
-    let clone = document.importNode(templateCategoriesSidebar.content, true);
-
-    Object.keys(categoriesSidebarData).forEach((key) => {
-      clone.querySelector(".item-sidebar").innerHTML =
-        categoriesSidebarData[key];
-    });
-    let moreCategoriesLink = document.querySelector(".more-categories-link");
-    let allCategoriesList = document.querySelector(".all-categories-list");
-    allCategoriesList.insertBefore(clone, moreCategoriesLink);
+  function renderProductsPage(pageNum) {
+    currentPage = pageNum;
+    renderProducts();
   }
 
-  for (let i = 0; i < 5; i++) {
-    productData.forEach((product) => {
-      let clone = document.importNode(templateProductCard.content, true);
-      console.log(product);
-      Object.keys(product).forEach((key) => {
-        if (key === "price") {
-          clone.querySelector(".product-price-current").innerHTML =
-            product[key];
-          clone.querySelector(".product-price-discount").innerHTML =
-            product[key] * 0.9;
-        } else {
-          clone.querySelector(".product-" + key).innerHTML = product[key];
-        }
+  function renderCategories() {
+    let templateCategory = document.querySelector(".category-item");
+
+    for (let i = 0; i < 5; i++) {
+      let clone = document.importNode(templateCategory.content, true);
+      Object.keys(categoriesData).forEach((key) => {
+        clone.querySelector(".item-" + key).textContent = categoriesData[key];
       });
-      let productsContainer = document.querySelector(".products-container");
-      productsContainer.appendChild(clone);
+
+      let productCategories = document.querySelector(".product-categories");
+      productCategories.appendChild(clone);
+    }
+  }
+
+  function renderSidebar() {
+    let templateCategoriesSidebar = document.querySelector(
+      ".all-categories-sidebar"
+    );
+
+    for (let i = 0; i < 8; i++) {
+      let clone = document.importNode(templateCategoriesSidebar.content, true);
+
+      Object.keys(categoriesSidebarData).forEach((key) => {
+        clone.querySelector(".item-sidebar").innerHTML =
+          categoriesSidebarData[key] + " (xxx)";
+      });
+
+      let moreCategoriesLink = document.querySelector(".more-categories-link");
+      let allCategoriesList = document.querySelector(".all-categories-list");
+      allCategoriesList.insertBefore(clone, moreCategoriesLink);
+    }
+  }
+
+  function renderProducts() {
+    let templateProductCard = document.querySelector(".product-card");
+    let productsContainer = document.querySelector(".products-container");
+    productsContainer.innerHTML = "";
+
+    let firstIndex = currentPage * currentPageSize - currentPageSize;
+    let lastIndex = currentPage * currentPageSize;
+
+    currentArray
+      .slice(firstIndex, lastIndex)
+      .filter((card) => {
+        if (!priceTo && !priceFrom) {
+          return true;
+        } else if (priceTo && !priceFrom) {
+          return priceTo >= card.price * discount;
+        } else if (!priceTo && priceFrom) {
+          return priceFrom <= card.price * discount;
+        } else {
+          return (
+            priceFrom <= card.price * discount &&
+            priceTo >= card.price * discount
+          );
+        }
+      })
+      .sort(currentSort)
+      .forEach((card) => {
+        let clone = document.importNode(templateProductCard.content, true);
+        Object.keys(card).forEach((key) => {
+          if (key === "price") {
+            clone.querySelector(".product-price-current").innerHTML =
+              card[key] + "$";
+            clone.querySelector(".product-price-discount").innerHTML =
+              card[key] * discount + "$";
+          } else {
+            clone.querySelector(".product-" + key).innerHTML = card[key];
+          }
+        });
+        productsContainer.appendChild(clone);
+      });
+  }
+
+  function onSearchChange(textValue) {
+    currentPage = 1;
+    currentArray = productData.filter((product) =>
+      product.name.toLowerCase().includes(textValue)
+    );
+  }
+
+  function initSearch() {
+    let productSearch = document.querySelector(".search-input");
+    productSearch.addEventListener("input", function (e) {
+      let textValue = this.value;
+      onSearchChange(textValue);
+      renderProducts();
+    });
+
+    let closeButton = document.querySelector(".button-close");
+    closeButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      productSearch.value = "";
+      onSearchChange("");
+      renderProducts();
     });
   }
-  let slider = document.getElementById("slider");
 
-  noUiSlider.cssClasses.target += " range-slider";
+  function initPagination() {
+    let pagination = document.querySelector(".pagination");
+    pagination.addEventListener("click", function (e) {
+      e.preventDefault();
+      let currentPage = e.target.dataset.page;
+      renderProductsPage(parseInt(currentPage));
+    });
+  }
 
-  noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-      min: 0,
-      max: 100,
-    },
-  });
+  function initPriceRange() {
+    let priceFromInput = document.querySelector(".input-from");
+    let priceToInput = document.querySelector(".input-to");
+    priceFromInput.addEventListener("input", function (e) {
+      priceFrom = parseFloat(priceFromInput.value);
+      renderProducts();
+    });
 
-  clearCheckboxButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    let checkboxProducts = document.querySelectorAll(".brand-item");
-    for (let i = 0; i < checkboxProducts.length; i++) {
-      checkboxProducts[i].checked = false;
-    }
-  });
+    priceToInput.addEventListener("input", function (e) {
+      priceTo = parseFloat(priceToInput.value);
+      renderProducts();
+    });
+  }
 });
